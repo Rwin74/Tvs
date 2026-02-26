d26a1794a0afd841b59fd3361e93b68b915b72d6
Atakan <atakan@example.com>
2026-02-26 21:54:52 +0300

    fix: update cookie path to root

--- admin-panel/app/api/auth/route.ts
+++ admin-panel/app/api/auth/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest) {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
                 sameSite: "lax",
-                path: "/admin",
+                path: "/",
                 maxAge: 60 * 60 * 24 * 365 * 10, // 10 years
             });
             return response;
